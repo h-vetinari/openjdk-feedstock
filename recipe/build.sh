ca2ef50e2bd0b91c49ee5ffeebf3cf03e57d6f7d
H. Vetinari <h.vetinari@gmx.com>
2026-07-04 17:04:27 +1100

    debug

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,8 @@ set -exuo pipefail
 
 JVM_BUILD_LOG_LEVEL=warn
 
+ls -ll .
+
 # Remove code signatures from osx-64 binaries as they will be invalidated in the later process.
 # TODO: Fix https://github.com/thefloweringash/sigtool to add --remove-signature support
 if [[ "${target_platform}" == "osx-64" ]]; then
